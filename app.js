b4a5f54e8e965ae2ce580df6de1599d3193e40b7
Aryan <chaudharyaryan3456@gmail.com>
2024-12-05 10:24:57 +0530

    updated

--- app.js
+++ app.js
@@ -3,6 +3,16 @@
 //     console.log(i);
 // }
 
-for(let i = 5; i >= 1; i--){
+// for(let i = 5; i >= 1; i--){
+//     console.log(i);
+// }
+
+// 2. Print all odd numbers (1 to 15)
+// for(let i = 1; i <= 15; i += 2){
+//         console.log(i);
+// }
+
+// 3. Print all even numbers (2 to 10)
+for(let i = 2; i <= 10; i += 2){
     console.log(i);
 }
