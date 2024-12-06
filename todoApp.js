c07a4b190d84d98ff663d8265e2c3fa8dfff3e53
Aryan <chaudharyaryan3456@gmail.com>
2024-12-06 18:46:36 +0530

    updated

--- todoApp.js
+++ todoApp.js
@@ -7,5 +7,15 @@ while(true){
         console.log("quitting app");
         break;
     }
+    if(req == "list"){
+        console.log("-------------------");
+        for(task of todo){
+            console.log(task);
+        }
+        console.log("-------------------");
+    }else if(req == "add"){
+        prompt("Please Enter the task you want to add : ");
+        todo.push(task);
+        console.log("task added");
+    }
 }
-
