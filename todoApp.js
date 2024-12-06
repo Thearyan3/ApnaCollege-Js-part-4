28d4e8cb94d32ddf37966a365459f9ef6688b427
Aryan <chaudharyaryan3456@gmail.com>
2024-12-06 19:08:14 +0530

    updated

--- todoApp.js
+++ todoApp.js
@@ -3,19 +3,26 @@ let todo = [];
 let req = prompt("Please Enter your request : ");
 
 while(true){
-    if(req == "quit"){
+    if(req == "quit") {
         console.log("quitting app");
         break;
     }
-    if(req == "list"){
+    if(req == "list") {
         console.log("-------------------");
-        for(task of todo){
-            console.log(task);
+        for(let i = 0; i < todo.length; i++){
+            console.log(i, todo[i]);
         }
         console.log("-------------------");
-    }else if(req == "add"){
-        prompt("Please Enter the task you want to add : ");
+    } else if(req == "add") {
+        let task = prompt("Please Enter the task you want to add : ");
         todo.push(task);
         console.log("task added");
+    } else if(req == "delete") {
+        let idx = prompt("Please Enter the index you want to delete : ");
+        todo.splice(idx, 1);
+        console.log("task deleted");
+    } else {
+        alert("Wrong request");
     }
+    req = prompt("Please Enter your request : ");
 }
